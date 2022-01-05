34d0ff35c3e031981d94832ba5976c9faaed219a
Oliver Blaser <kontakt@oblaser.ch>
2022-01-05 23:20:36 +0100

    file headers and index.json

--- src/_resources/ojs.js
+++ src/_resources/ojs.js
@@ -1,26 +1,28 @@
 /*
-
-author         Oliver Blaser
-date           26.06.2021
-copyright      GNU GPLv3 - Copyright (c) 2021 Oliver Blaser
-
+author          Oliver Blaser
+date            05.01.2022
+copyright       MIT - Copyright (c) 2022 Oliver Blaser
 */
 
 /*
-Copyright (C) 2021 Oliver Blaser
+Copyright (c) 2022 Oliver Blaser
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 function ojs_roundSignificant(value, nDigits = 4)
@@ -76,6 +78,8 @@ function ojs_expFormatEng(value, nSigDig = 4)
 }
 
 //#p rm
+
+// ojs tests
 /*
 console.log(ojs_roundSignificant(0));
 console.log(ojs_roundSignificant(123.456));
@@ -108,5 +112,5 @@ console.log(ojs_expFormatEng(Infinity));
 console.log(ojs_expFormatEng(-Infinity));
 console.log(ojs_expFormatEng(1/0));
 console.log(ojs_expFormatEng(-1/0));
-*/
+/**/
 //#p endrm
--- src/calculators/resistor-networks/index.js
+++ src/calculators/resistor-networks/index.js
@@ -1,26 +1,7 @@
 /*
-
-author         Oliver Blaser
-date           26.06.2021
-copyright      GNU GPLv3 - Copyright (c) 2021 Oliver Blaser
-
-*/
-
-/*
-Copyright (C) 2021 Oliver Blaser
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+author          Oliver Blaser
+date            26.06.2021
+copyright       GNU GPLv3 - Copyright (c) 2021 Oliver Blaser
 */
 
 function amp(ra, rb) { return (ra+rb)/rb; } // ratio = V1/V2 = (Ra+Rb)/Rb
